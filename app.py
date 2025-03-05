2181035152ae177911ff35672bacdefd4d41f27a
QuantumCore (AI) by urobotsoftware <quantumcore8@gmail.com>
2025-03-05 20:30:59 -0300

    refactor(app.py): add comments and clean up code

--- app.py
+++ app.py
@@ -12,11 +12,13 @@ app.permanent_session_lifetime = timedelta(minutes=60)
 
 @app.context_processor
 def inject_static_url():
+    # Adiciona o URL estático a todos os templates
     return dict(static_url=url_for('static', filename=''))
 
 def login_required(f):
     @wraps(f)
     def decorated_function(*args, **kwargs):
+        # Redireciona para a página de login se o usuário não estiver autenticado
         if "user" not in session:
             return redirect(url_for("login"))
         return f(*args, **kwargs)
@@ -24,6 +26,7 @@ def login_required(f):
 
 @app.route('/')
 def index():
+    # Determina a versão da página a ser renderizada com base no tipo de dispositivo
     ua_string = request.headers.get('User-Agent')
     user_agent = parse(ua_string)
     if user_agent.is_tablet:
@@ -37,6 +40,7 @@ def index():
 def plan_contentcreator_checkout():
     ua_string = request.headers.get('User-Agent')
     user_agent = parse(ua_string)
+    # Renderiza a página de checkout apropriada para cada tipo de dispositivo
     if user_agent.is_pc:
         return render_template('LandingPage_Checkout_Desktop.html')
     elif user_agent.is_mobile:
@@ -46,6 +50,7 @@ def plan_contentcreator_checkout():
 def plan_studio_checkout():
     ua_string = request.headers.get('User-Agent')
     user_agent = parse(ua_string)
+    # Renderiza a página de checkout apropriada para cada tipo de dispositivo
     if user_agent.is_pc:
         return render_template('LandingPage_Checkout_Desktop.html')
     elif user_agent.is_mobile:
@@ -55,6 +60,7 @@ def plan_studio_checkout():
 def checkout_sucess():
     ua_string = request.headers.get('User-Agent')
     user_agent = parse(ua_string)
+    # Renderiza a página de sucesso de checkout para cada tipo de dispositivo
     if user_agent.is_pc:
         return render_template('LandingPage_Checkout_Desktop_Sucess_Page.html')
     elif user_agent.is_mobile:
@@ -64,16 +70,12 @@ def checkout_sucess():
 def checkout_cancel():
     ua_string = request.headers.get('User-Agent')
     user_agent = parse(ua_string)
+    # Renderiza a página de cancelamento de checkout para cada tipo de dispositivo
     if user_agent.is_pc:
         return render_template('LandingPage_Checkout_Desktop_Cancel_Page.html')
     elif user_agent.is_mobile:
         return render_template('LandingPage_Checkout_Mobile_Cancel_Page.html')
 
-
-
-
-
-
+# Quando você estiver pronto para implantar a aplicação, descomente a linha abaixo
 # if __name__ == '__main__':
 #     serve(app, host="0.0.0.0", port=800)
-
