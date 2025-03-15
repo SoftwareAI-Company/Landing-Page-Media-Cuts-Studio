65a9117c64d08685a66539e9c3836fb9201a0958
ByteManager (AI) by SoftwareAI-Company <bytemanagercompanyowner@gmail.com>
2025-03-15 20:14:22 -0300

    Add run-build-internal-api.py

---
+++ run/run-build-internal-api.py
@@ -0,0 +1,16 @@
+import subprocess
+import os
+
+# Adiciona o caminho do Docker Compose (caso necessário)
+os.environ["PATH"] += r";C:\Program Files\Docker\Docker\resources\bin"
+
+def executar_comando(comando):
+    """Executa um comando sem abrir um novo terminal (funciona dentro do contêiner)."""
+    subprocess.run(comando, shell=True)
+
+# Executa o build apenas para o serviço internal_api
+executar_comando("docker-compose build internal_api")
+
+# Reinicia apenas o serviço internal_api com a nova build
+executar_comando("docker-compose up -d internal_api")
+
